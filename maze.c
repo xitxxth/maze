f2610354377f0233e8a91036580eae04aa4965b9
xitxxth <jongyn1128@gmail.com>
2022-05-27 02:22:58 +0900

    file in_out o

--- maze.c
+++ maze.c
@@ -1,8 +1,30 @@
 #include "maze.h"
 int main(void)
 {
+    int N, M, i, j, hole;
+    scanf("%d", &N);
+    scanf("%d", &M);
+    int WIDTH = 2*N+1;
+    int HEIGHT = 2*M+1;
+    char field[WIDTH][HEIGHT];
+    for(i=0; i<HEIGHT; i+=2)
+        for(j=0; j<WIDTH; j+=2)
+            field[i][j] = CROSS;
+    for(i=0; i<HEIGHT; i+=2)
+        for(j=1; j<WIDTH; j+=2)
+            field[i][j] = HORIZONTAL;
+    for(i=1; i<HEIGHT; i+=2)
+        for(j=0; j<WIDTH; j+=2)
+            field[i][j] = VERTICAL;
+    for(i=1; i<HEIGHT; i+=2)
+        for(j=1; j<WIDTH; j+=2)
+            field[i][j] = EMTPY;
+    FILE* fp = fopen("20201604.maz","wt");
+    if(!fp){
+        printf("FILE OPEN FAILED\n");
+        exit(0);
+    }
     srand(time(NULL));
-    Init_Field(); //initiate field condition
     for(int i=1; i<HEIGHT; i+=2){
         for(int j=1; j<WIDTH; j+=2){
             hole = rand()%2;
@@ -16,9 +38,14 @@ int main(void)
 
     //final print
     for(int i=0; i<HEIGHT; i++){
-        printf("\n");
-        for(int j=0; j<WIDTH; j++)
-            printf("%c", field[i][j]);
+        for(int j=0; j<WIDTH; j++){
+            fprintf(fp, "%c", field[i][j]);
+        }
+        fprintf(fp, "\n");
+    }
+    if(fclose(fp)){
+        printf("FILE CLOSE ERROR\n");
+        exit(0);
     }
     return 0;
 }
--- maze.h
+++ maze.h
@@ -7,32 +7,9 @@
 #define HORIZONTAL '-'
 #define VERTICAL '|'
 #define EMTPY ' '
-
-#define HEIGHT 11
-#define WIDTH 13
 enum Hole_Direction {
 DOWN = 0,
 RIGHT,
 NONE
 };
 /////////////////////////
-char field[HEIGHT][WIDTH];
-int hole;
-/////////////////////////
-void Init_Field(void);
-void Init_Field(void){
-    int i, j;
-    for(i=0; i<HEIGHT; i+=2)
-        for(j=0; j<WIDTH; j+=2)
-            field[i][j] = CROSS;
-    for(i=0; i<HEIGHT; i+=2)
-        for(j=1; j<WIDTH; j+=2)
-            field[i][j] = HORIZONTAL;
-    for(i=1; i<HEIGHT; i+=2)
-        for(j=0; j<WIDTH; j+=2)
-            field[i][j] = VERTICAL;
-    for(i=1; i<HEIGHT; i+=2)
-        for(j=1; j<WIDTH; j+=2)
-            field[i][j] = EMTPY;
-    return;
-}
